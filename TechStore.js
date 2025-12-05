f7f5b3dc62331bc7d3690f71595fa8cc4fa127e8
Rebeca-Soares <rebecasoaresc.eng@gmail.com>
2025-12-05 12:56:32 +0000

    Finalizando projeto

--- TechStore.js
+++ TechStore.js
@@ -29,30 +29,28 @@ const inventario = [
     quantidade: 1,
   },
 ];
-console.log(inventario);
-
-// 1.
-console.log("\n-1------");
 
 function atualizarPreco(id, novoPreco) {
   const valor = inventario.find((produto) => produto.id === id);
 
   if (valor) {
     valor.preco = novoPreco;
-    console.log(valor.produto, "novo preço: €", novoPreco);
+    return valor;
   }
+
+  return null
 }
-
-atualizarPreco(2, 900);
-
-//2.
-console.log("\n--2------");
 
 let historicoVendas = [];
 
 function registarVenda(quantidadeEscolhida, nome) {
 
   const item = inventario.find(produto  => produto.produto === nome);
+
+  if (!item) {
+    console.log(nome, 'não encontrado no inventário!');
+    return null;
+  };
   
   if(quantidadeEscolhida <= item.quantidade){
 
@@ -67,34 +65,23 @@ function registarVenda(quantidadeEscolhida, nome) {
 
       item.quantidade -= quantidadeEscolhida;
 
-      console.log(`Produto Vendido: ${nome} - Qnt: ${quantidadeEscolhida} UN - Desconto ${desconto} - Preço Total: ${total}.`);
+      const venda = { produto: nome, quantidade: quantidadeEscolhida, total, desconto };
+      historicoVendas.push(venda);
 
-      historicoVendas.push({produto: nome, quantidade: quantidadeEscolhida, total, desconto});
+    return venda;
 
   } else {
       console.log(nome, 'Sem Stock!');
   }
 }
-
-registarVenda(9, 'Portatil');
-registarVenda(1, 'Ipad');
-registarVenda(1, 'Coluna de Som');
-
-console.log('--Hisrotico de vendas--');
-
-historicoVendas.forEach(venda => {
-  console.log(`Produto: ${venda.produto} - Qnt: ${venda.quantidade} - Total: €${venda.total.toFixed(2)} - Desconto Aplicado: €${venda.desconto.toFixed(2)}`);
+function mostrarHistoricoVendas(){
+  historicoVendas.forEach(venda => { console.log(`Produto: ${venda.produto} - Qnt: ${venda.quantidade} - Total: €${venda.total.toFixed(2)} - Desconto Aplicado: €${venda.desconto.toFixed(2)}`);
 });
+}
 
-//3.
-console.log("\n--3------");
-const totalInventario = inventario.reduce(
-  (acc, n) => acc + n.preco * n.quantidade, 0);
-console.log(`O total do inventário é: ${totalInventario.toFixed(2)} EUR`);
-console.log(inventario);
-
-//4.
-console.log("\n--4------");
+function totalInventario() {
+  return inventario.reduce((acc, n) => acc + n.preco * n.quantidade, 0);
+}
 
 function limpezaStock() {
   for (let i = 0; i < inventario.length; i++) {
@@ -102,19 +89,13 @@ function limpezaStock() {
       inventario.splice(i, 1);
     } 
   }
-
-  console.log("Inventário atualizado:", inventario);
+  return inventario;
 }
 
-limpezaStock();
-
-//5. Filtro de Categoria
-console.log("\n--5------");
-const audio = inventario.filter((i) => i.categoria === "Som");
-console.log(audio);
-
-//6. Produto "Premium"
-console.log("\n--6------");
+function filtrarCategoria (categoriaProduto){
+    let cat = inventario.filter((i) => i.categoria=== categoriaProduto);
+    return cat
+}
 
 function produtoPremium() {
   return inventario.reduce((produtoMaisCaro, produtoAtual) => {
@@ -122,42 +103,44 @@ function produtoPremium() {
   });
 }
 
-produtoPremium();
-
-console.log(produtoPremium());
-
-//7. Repor Stock
-console.log("\n--7------");
-
 function reporStock(nomeProduto, adicionarProduto) {
   const unidade = inventario.find((produto) => produto.produto === nomeProduto);
 
   if (unidade) {
     unidade.quantidade += adicionarProduto;
-    console.log(`Produto: ${unidade.produto} -  Adicionado: ${adicionarProduto} - Total: ${unidade.quantidade}`);
+    return unidade
   }
+  return null
 }
-
-reporStock("Coluna de Som", 90);
-
-//8. Listar Produtos
-console.log("\n--8------");
 
 function listarProdutos() {
-  inventario.forEach((p) => console.log(`${p.produto} - quantidade: ${p.quantidade}`));
+  const lista = [];
+  inventario.forEach((p) => lista.push(`${p.produto} - quantidade: ${p.quantidade}`));
+  return lista;
 }
 
-listarProdutos();
+function main() {
+  atualizarPreco(2, 900);
+  registarVenda(9, "Portatil");
+  registarVenda(1, "Ipad");
+  registarVenda(1, "Coluna de Som");
+  registarVenda(1, "Mouse");
+  mostrarHistoricoVendas();
+  produtoPremium();
+  totalInventario();
+  reporStock("Coluna de Som", 90);
+  listarProdutos();
+  limpezaStock();
+  filtrarCategoria('Som');
 
-//9. Adicionar Produto Novo:
-console.log("\n--9------");
-inventario.push({
-  id: 6,
-  categoria: "Fotografia",
-  produto: "Papel fotográfico",
-  preco: 15,
-  quantidade: 25,
-});
+  inventario.push({
+    id: 6,
+    categoria: "Fotografia",
+    produto: "Papel fotográfico",
+    preco: 15,
+    quantidade: 25,
+  });
+}
 
-console.log(inventario);
+main();
 
